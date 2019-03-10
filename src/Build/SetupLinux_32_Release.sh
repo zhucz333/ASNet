f4796fb5f427fe3b3fca6bc5db19aa6f3c78df87
zhucz333 <zhucz333@163.com>
2019-03-11 02:15:10 +0800

    ADD CMAKE FILES

---
+++ src/Build/SetupLinux_32_Release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+workdir=$(cd $(dirname $0); pwd)
+
+buildDir=$workdir/../build_32_Release
+
+if [ ! -d $buildDir ];then
+	mkdir -p $buildDir
+fi
+
+cd $buildDir
+
+echo Setup Linux 32 bit Release Solution in $(pwd)
+
+cmake -DUSE_32BITS=ON -DCMAKE_BUILD_TYPE=Release ../ASNet 
